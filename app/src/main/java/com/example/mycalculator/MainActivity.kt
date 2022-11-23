b89c9c25b36f5ffbc14b98e23e6862d54bdee6f0
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2022-11-24 00:10:38 +0530

    Explanation of what i have done so far Comments added :)

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -13,21 +13,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    private var final: Double = 0.0
-    private var fint: Int = 0
-    private var op = ""
-    private var old = ""
-    private var isnewop = false
-    private var nums = ""
-    private var num = ""
-    private var numblck:Boolean=false
-    fun button(view: View) {
-        val buselected = view as Button
-        if (isnewop) {
+
+    private var final: Double = 0.0             //Final float Value
+    private var fint: Int = 0                       //Final int Value
+    private var op = ""                         // Current Operator
+    private var old = ""                            // Number before user entered 2nd operator
+    private var isnewop = false                 //is there any new operator??
+    private var nums = ""                           //NUMBER which is entered by user after 2nd operator
+    private var num = ""                        // Number Shown in textView
+    private var numblck:Boolean=false               //UNBlocks Number buttons // Used so the user can't edit number shown in textview after he clicks equal
+
+    //Checking Which Number Has been Clicked By the user
+
+    fun button(view: View) {                    // Passing view (Which is Button)
+        val buselected = view as Button             //Variable Defining View As a button For future usage
+        if (isnewop) {                         // if there is new operator than the nums will be 0 to get new Values by user
             nums = ""
             isnewop = false
         }
-        if(numblck){
+        if(numblck){                                // Checks if numblck is true and resets  both ui texts and (old values by user, and final answers)
             binding.textView.text=""
             binding.textView2.text=""
             old=""
@@ -80,32 +84,36 @@ class MainActivity : AppCompatActivity() {
                 num += "."
                 nums += "."
             }
-            binding.pnm.id -> {
+            binding.pnm.id -> {                         // If user press =/- button thn changes num according to it
                 if (num.contains("-") || nums.contains("-")) {
                     num = num.removePrefix("-")
                     nums = nums.removePrefix("-")
                 } else {
-                    num = "-" + num
-                    nums = "-" + nums
+                    num = "-$num"
+                    nums = "-$nums"
                 }
             }
-            binding.rev.id -> {
+            binding.rev.id -> {                             // Remove last value of num when pressed back button
                 num = num.removeSuffix(num.last().toString())
                 nums = nums.removeSuffix(nums.last().toString())
 
             }
 
         }
-        numblck=false
-        binding.textView2.text = num
+        numblck=false                                     // changing numblock back to false as we now want user to enter more numbers not only a single number
+        binding.textView2.text = num                          //Setting ui text to num
 
 
     }
 
+            // Checking Which Operator has User pressed
 
     fun action(view: View) {
         val buselected = view as Button
-        isnewop = true
+        isnewop = true                                 //enable us to enter new number as it will reset the nums value shown in above function
+
+        // Checks if the user has Clicked new operator just after getting a final answer or after he has clicked equal
+        // Then we set num or text of ui to the final answer
         if (fint!=0){
             binding.textView2.text=fint.toString()
             fint=0
@@ -114,12 +122,14 @@ class MainActivity : AppCompatActivity() {
             binding.textView2.text=final.toString()
             final=0.0
         }
-        var num = binding.textView2.text.toString()
+        //
+
+        var num = binding.textView2.text.toString()                 //we set num now by the help of ui text this solve both without clicked equal and after we clicked equal
         if (old.isEmpty()) {
-            old = binding.textView2.text.toString()
+            old = binding.textView2.text.toString()                     // Time to take old value which is present before new operator
         }
         numblck=true
-        if(numblck){
+        if(numblck){                             //Resets nums value to 0 as now we will take new values to add into the old
             nums=""
             numblck=false
         }
@@ -132,8 +142,8 @@ class MainActivity : AppCompatActivity() {
                         num += "+"
                         nums=""
                     } else {
-                        if(num.last().toString()==op){
-                            num=num.removeSuffix(op)
+                        if(num.last().toString()==op){                         //if the user has clicked different operator after clicking an operator thn we delete the previous
+                            num=num.removeSuffix(op)                             //operator from num and update it with new operator viceversa with op
                             op="+"
                             num+="+"
                         }
@@ -199,6 +209,11 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+     //Equal buttons give us the final answer
+
+
+    //simply check if there is float present in old or nums thn give us float value else integer values
+
     fun equal(view: View) {
         var counter = 0
         fint = 0
@@ -246,7 +261,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        if (counter > 0) {
+        if (counter > 0) {                            //Checks if float or int to show as answer and resets variables(Mentioned Below), sets numblck true to prevent changes in num
             binding.textView.text = final.toString()
             old=""
             op=""
@@ -262,7 +277,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun ac(view: View) {
+    fun ac(view: View) {                //Resets everything to its Starting Value ac Button
         nums = ""
         old = ""
         num = ""
